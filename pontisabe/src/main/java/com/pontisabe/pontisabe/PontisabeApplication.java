0007a7bcfb82d618885a9b0b7cbf0e8685d28e2f
Juan Rodriguez <juanloklo31@gmail.com>
2024-10-22 00:07:18 +0000

    Proyecto Inicial

---
+++ pontisabe/src/main/java/com/pontisabe/pontisabe/PontisabeApplication.java
@@ -0,0 +1,13 @@
+package com.pontisabe.pontisabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontisabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontisabeApplication.class, args);
+	}
+
+}
---
+++ pontisabe/src/test/java/com/pontisabe/pontisabe/PontisabeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontisabe.pontisabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontisabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
