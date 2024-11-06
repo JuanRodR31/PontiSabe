e600ac417858f47e28282afe41a674cb4c20d57e
Juan Rodriguez <juanloklo31@gmail.com>
2024-11-06 13:10:05 +0000

    Cambios V8 2

--- pontisabe/src/main/java/com/pontisabe/pontisabe/DatabaseManagement/DatabaseConnection.java
+++ pontisabe/src/main/java/com/pontisabe/pontisabe/DatabaseManagement/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/pontisabe_db";
+    private static final String URL = "jdbc:h2:mem:testdb";
     private static final String USER = "pontisabe_user";
     private static final String PASSWORD = "secret";
 
@@ -13,3 +13,17 @@ public class DatabaseConnection {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+// public class DatabaseConnection {
+//     private static final String URL = "jdbc:mysql://localhost:3306/pontisabe_db";
+//     private static final String USER = "pontisabe_user";
+//     private static final String PASSWORD = "secret";
+
+//     public static Connection getConnection() throws SQLException {
+//         return DriverManager.getConnection(URL, USER, PASSWORD);
+//     }
+// }
+// spring.datasource.url=jdbc:h2:mem:testdb
+// spring.datasource.driverClassName=org.h2.Driver
+// spring.datasource.username=sa
+// spring.datasource.password=password
+// spring.jpa.database-platform=org.hibernate.dialect.H2Dialect
