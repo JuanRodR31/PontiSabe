100fcdd8183c61b7c6639c25db9879f14f98452c
Juan Rodriguez <juanloklo31@gmail.com>
2024-11-19 12:56:19 +0000

    prueba updatebd

--- pontisabe/src/main/java/com/pontisabe/pontisabe/Entities/User.java
+++ pontisabe/src/main/java/com/pontisabe/pontisabe/Entities/User.java
@@ -25,7 +25,7 @@ public class User {
     @Column(nullable = false, unique = true, length = 100)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String password;
 
     @OneToMany(mappedBy = "follower", cascade = CascadeType.ALL, orphanRemoval = true)
